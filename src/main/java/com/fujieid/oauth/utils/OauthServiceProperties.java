02a43506f31262b29377feb3afb0784a460ce318
lapati5 <1352059320@qq.com>
2021-08-13 10:39:51 +0800

    oauth-service commit05

--- src/main/java/com/fujieid/oauth/utils/OauthServiceProperties.java
+++ src/main/java/com/fujieid/oauth/utils/OauthServiceProperties.java
@@ -1,10 +1,8 @@
 package com.fujieid.oauth.utils;
 
 import lombok.Data;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 /**
