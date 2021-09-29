925963f5970763c153c5f8d8f33f758ca1d6960e
lapati5 <1352059320@qq.com>
2021-09-29 11:50:06 +0800

    代码Bug修复

--- src/main/java/com/fujieid/oauth/utils/JapIdsConstUtil.java
+++ src/main/java/com/fujieid/oauth/utils/JapIdsConstUtil.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
 import java.util.Map;
 
 /**
